0619741c65351405e0f2d92dee3c855b93cf1b0d
DavidKocen <dkocen0@gmail.com>
2019-09-27 14:56:23 +0100

    again changed so now it is a formal class called BinaryTree instead of just functions. Makes testing easier

--- binaryTree.py
+++ binaryTree.py
@@ -4,53 +4,54 @@ class Node:
         self.left = None
         self.right = None
 
+class BinaryTree:
+    def __init__(self, data):
+        self.root = self.construct_bt(data)
+        self.inorder = self.inorder(self.root)
 
-def create_node(data, i, created, root):
-    if created[i] is not None:
-        return
+    def create_node(self, data, i, created, root):
+        if created[i] is not None:
+            return
 
-    created[i] = Node(i)
+        created[i] = Node(i)
 
-    if data[i] == -1:
-        root[0] = created[i]
-        return
+        if data[i] == -1:
+            root[0] = created[i]
+            return
 
-    if created[data[i]] is None:
-        create_node(data, data[i], created, root)
+        if created[data[i]] is None:
+            self.create_node(data, data[i], created, root)
 
-    p = created[data[i]]
+        p = created[data[i]]
 
-    if p.left is None:
-        p. left = created[i]
-    else:
-        p.right = created[i]
+        if p.left is None:
+            p. left = created[i]
+        else:
+            p.right = created[i]
 
+    def construct_bt(self, data):
+        n = len(data)
+        created = [None for i in range(n + 1)]
 
-def construct_bt(data):
-    n = len(data)
-    created = [None for i in range(n + 1)]
+        root = [None]
+        for i in range(n):
+            self.create_node(data, i, created, root)
 
-    root = [None]
-    for i in range(n):
-        create_node(data, i, created, root)
+        return root[0]
 
-    return root[0]
+    def inorder(self, root):
+        if root is None:
+            return []
 
-def inorder(root):
-    if root is None:
-        return []
-
-    left_list = inorder(root.left)
-    right_list = inorder(root.right)
-    return left_list + [root.key] + right_list
+        left_list = self.inorder(root.left)
+        right_list = self.inorder(root.right)
+        return left_list + [root.key] + right_list
 
 
 
 if __name__ == '__main__':
     data = [-1, 0, 0, 1, 1, 3, 5]
-    root = construct_bt(data)
-    print('Inorder traversal of constructed tree:')
-    print(inorder(root))
-
+    bt= BinaryTree(data)
+    print(bt.inorder)
 
 
