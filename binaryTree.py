be504620155b5b41d5883fe86fa5519ca803d305
DavidKocen <dkocen0@gmail.com>
2019-09-27 14:24:37 +0100

    file containing functions for constructing a binary tree and a simple driver function

---
+++ binaryTree.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, value):
+        self.key = value
+        self.left = None
+        self.right = None
+
+
+def create_node(data, i, created, root):
+    if created[i] is not None:
+        return
+
+    created[i] = Node(i)
+
+    if data[i] == -1:
+        root[0] = created[i]
+        return
+
+    if created[data[i]] is None:
+        create_node(data, data[i], created, root)
+
+    p = created[data[i]]
+
+    if p.left is None:
+        p. left = created[i]
+    else:
+        p.right = created[i]
+
+
+def construct_bt(data):
+    n = len(data)
+    created = [None for i in range(n + 1)]
+
+    root = [None]
+    for i in range(n):
+        create_node(data, i, created, root)
+    return root[0]
+
+
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print(root.key, end = ' ')
+        inorder(root.right)
+
+
+if __name__ == '__main__':
+    data = [-1, 0, 0, 1, 1, 3, 5]
+    root = construct_bt(data)
+    print('Inorder traversal of constructed tree:')
+    inorder(root)
+
+
