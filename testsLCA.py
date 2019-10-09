1b333377123ddba638b4cca48acea9df19f09977
DavidKocen <dkocen0@gmail.com>
2019-10-09 17:30:29 +0100

    added more tests. Right now they all fail but will implement new code later

--- testsLCA.py
+++ testsLCA.py
@@ -5,10 +5,13 @@ from binarytree import tree, build, Node
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
+        """Just a test to make sure it works. Should always pass"""
+
         self.assertEqual(True, True)
 
     def test_empty_tree(self):
         """Test that an empty tree returns None for LCA"""
+
         root = None
         lca = LCA.findLCA(root, root, root)
         self.assertEqual(lca, None, f'Incorrect LCA: {lca}. Should be {None}')
@@ -30,6 +33,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_straight_left(self):
         """Tests a all left node tree (essentially a linked list)"""
+
         values = [0, 1, None, 2, None, None, None, 3, None, None, None, None, None, None, None]
 
         root = build(values)
@@ -39,6 +43,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_straight_right(self):
         """Tests a all right node tree"""
+
         values = [0, None, 1, None, None, None, 2, None, None, None, None, None, None, None, 3]
 
         root = build(values)
@@ -48,6 +53,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_null_leaf(self):
         """Tests tree where one node is None. Should return non-None leaf"""
+
         values = [0, 1, 2, 3, None]
 
         root = build(values)
@@ -57,6 +63,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_balanced_tree(self):
         """Tests a balanced tree based on generated list of 100 random numbers online"""
+
         values = [10, 12, 14, 24, 39, 50, 54, 62, 67, 92, 134, 153, 161, 162, 167, 171, 172, 173, 191, 193, 197, 205,
                   206, 208, 210, 211, 222, 239, 240, 246, 249, 253, 263, 270, 286, 299, 303, 305, 308, 316, 319, 325,
                   344, 349, 350, 353, 363, 365, 371, 395, 414, 437, 453, 459, 463, 489, 500, 503, 512, 514, 515, 516,
@@ -76,6 +83,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_unbalanced_tree(self):
         """Tests an unbalanced tree based on random binary tree generator online"""
+
         root = Node(67)
         root.left = Node(11)
         root.right = Node(82)
@@ -98,6 +106,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_simple_dag(self):
         """Tests a basic DAG"""
+
         graph = [[0, 1], [0, 2], [1, 3], [2, 3], [1, 4]]
         lca1 = LCA.findLCA(graph, 1, 2)
         lca2 = LCA.findLCA(graph, 1, 3)
@@ -109,8 +118,35 @@ class MyTestCase(unittest.TestCase):
 
     def test_slides_dag(self):
         """Tests DAG given in slides"""
-        graph = [[0, 1], [0, 2], [1, 3], [2, 4], [3, 5], [4, 5], [5, 6]]
 
+        graph = [[0, 1], [0, 2], [1, 3], [2, 4], [3, 5], [4, 5], [5, 6]]
+        lca1 = LCA.findLCA(graph, 3, 2)
+        lca2 = LCA.findLCA(graph, 0, 6)
+        lca3 = LCA.findLCA(graph, 3, 5)
+        lca4 = LCA.findLCA(graph, 3, 6, 2)
+
+        self.assertEqual(lca1, 0, f'Incorrect LCA {lca1}. Should be {0}')
+        self.assertEqual(lca2, 0, f'Incorrect LCA {lca2}. Should be {0}')
+        self.assertEqual(lca3, 3, f'Incorrect LCA {lca3}. Should be {3}')
+        self.assertEqual(lca4, 0, f'Incorrect LCA {lca4}. Should be {0}')
+
+    def test_complex_dag(self):
+        """Tests a complex DAG found online"""
+
+        graph = [[0, 2], [0, 12], [0, 14], [1, 11], [1, 16], [1, 13], [2, 6], [2, 13], [2, 16], [3, 4], [3, 7], [3, 8],
+                 [3, 13], [3, 14], [3, 16], [4, 7], [4, 15], [4, 18], [5, 10], [5, 16], [6, 18], [7, 8], [8, 13],
+                 [8, 16], [8, 17], [9, 17], [9, 18], [10, 11], [10, 13], [10, 17], [11, 16], [11, 17], [12, 18],
+                 [12, 19], [13, 14], [13, 15], [13, 18], [14, 19], [17, 18]]
+
+        lca1 = LCA.findLCA(graph, 0, 19) # Should be 0
+        lca2 = LCA.findLCA(graph, 17, 15) # Should be 8
+        lca3 = LCA.findLCA(graph, 5, 10, 19)  # Should be 5
+        lca4 = LCA.findLCA(graph, 1, 14, 18) # Should be 0
+
+        self.assertEqual(lca1, 0, f'Incorrect LCA {lca1}. Should be {0}')
+        self.assertEqual(lca2, 8, f'Incorrect LCA {lca2}. Should be {8}')
+        self.assertEqual(lca3, 5, f'Incorrect LCA {lca3}. Should be {5}')
+        self.assertEqual(lca4, 0, f'Incorrect LCA {lca4}. Should be {0}')
 
 
 if __name__ == '__main__':
